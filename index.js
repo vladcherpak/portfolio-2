468ff262d2cdd0bb745b4e03ad1c1f1cf2a99a3f
vladcherpak <cherpakvl@gmail.com>
2023-07-24 19:20:07 +0300

    finish adaptive

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const menuBtn= document.querySelector(".menu__btn")
-const navList= document.querySelector(".nav__list")
-console.log(menuBtn)
-menuBtn.addEventListener("click",()=>{
-    menuBtn.classList.toggle("active")
-    navList.classList.toggle("nav__active")
-})
+const menuBtn = document.querySelector(".menu__btn");
+const navList = document.querySelector(".nav__list");
+console.log(menuBtn);
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("active");
+  navList.classList.toggle("nav__active");
+});
